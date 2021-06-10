bbb0cb9f8da0b96a882740869abcea54cf5a2931
Piotr Idzik <vil02@o2.pl>
2021-06-10 20:30:27 +0200

    renamed to nowhere_convex

--- ubuntu_local_build.sh
+++ ubuntu_local_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-readonly FILE_NAME="not_convex_with_convex_bound"
+readonly FILE_NAME="nowhere_convex"
 readonly FILE_EXT=".tex"
 pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
 bibtex $FILE_NAME
