8b69218b62c007d7f34a0e49ef7f50da1b64bdf0
piotr.idzik <vil02@o2.pl>
2021-06-22 20:15:21 +0200

    lock added

--- ubuntu_local_build.sh
+++ ubuntu_local_build.sh
@@ -1,8 +1,55 @@
 #!/bin/bash
 
-readonly FILE_NAME="nowhere_convex"
-readonly FILE_EXT=".tex"
-pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
-bibtex $FILE_NAME
-pdflatex -interaction=batchmode -draftmode "$FILE_NAME$FILE_EXT"
-pdflatex -interaction=batchmode "$FILE_NAME$FILE_EXT"
+set -e
+set -u
+
+script_lock=$(dirname "${0}")/"build_lock_dir"
+readonly script_lock
+
+declare -r file_name="nowhere_convex"
+declare -r file_ext=".tex"
+
+function is_already_running()
+{
+    declare -i return_value=0
+    test -d "${script_lock}" ||
+    {
+        return_value=1
+    }
+    return "${return_value}"
+}
+ 
+ 
+function create_lock()
+{
+    mkdir "${script_lock}" ||
+    {
+        printf "Can not create lock\n"
+        exit 2
+    }
+}
+ 
+function remove_lock() 
+{
+    rm -rf "${script_lock}" ||
+    {
+        printf "Can not remove lock\n"
+        exit 3  
+    }
+}
+
+if is_already_running
+then
+    printf "Can not acquire lock (another instance is running?) - exiting.\n"
+    exit 1
+fi
+
+create_lock
+
+pdflatex -interaction=batchmode -draftmode "$file_name$file_ext"
+bibtex $file_name
+pdflatex -interaction=batchmode -draftmode "$file_name$file_ext"
+pdflatex -interaction=batchmode "$file_name$file_ext"
+
+remove_lock
+
